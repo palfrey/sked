9f3b17f224a1270fa79c8321c38823929fe1cbe2
Tom Parker <palfrey@tevp.net>
2018-01-01 22:08:52 +0000

    Fix ALLOWED_HOSTS and SECRET_KEY for Heroku

--- sked/settings.py
+++ sked/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9=@sc0f1ig1n&ro#otp42_8#3ci9ytc878h(h*-jh0o6n96oc^'
+SECRET_KEY = os.environ.get("SECRET_KEY", '9=@sc0f1ig1n&ro#otp42_8#3ci9ytc878h(h*-jh0o6n96oc^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "cal-merge.herokuapp.com"]
 
 
 # Application definition
