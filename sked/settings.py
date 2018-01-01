90a0140f0697ecad72d8483a35941b48522ec4a1
Tom Parker <palfrey@tevp.net>
2018-01-01 22:04:11 +0000

    Add STATIC_ROOT

--- sked/settings.py
+++ sked/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 try:
     from .settings_private import *
