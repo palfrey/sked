4bd2930383097a967d8fc4e616ec9ffef8204c58
Tom Parker <palfrey@tevp.net>
2018-05-06 23:24:16 +0100

    "Add" -> "Create" for merged calendar
    Fixes #4

--- calendars/forms.py
+++ calendars/forms.py
@@ -4,4 +4,4 @@ class NewCalendarForm(forms.Form):
     calendar_url = forms.URLField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Calendar URL'}))
 
 class MergedCalendarForm(forms.Form):
-    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Calendar name (possibly name of person to share it with)'}))
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Calendar name (possibly name of the person this will be shared with)'}))
