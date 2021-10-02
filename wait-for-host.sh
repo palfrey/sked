184576e339ffd6b7347c05d034f3b419818b931f
Tom Parker-Shemilt <palfrey@tevp.net>
2021-10-02 14:16:39 +0100

    Cope with failure on curl

--- wait-for-host.sh
+++ wait-for-host.sh
@@ -5,9 +5,9 @@ declare HOST=$1
 declare STATUS=$2
 declare TIMEOUT=$3
 
-HOST=$HOST STATUS=$STATUS timeout --foreground -s TERM $TIMEOUT bash -c \
+HOST=$HOST STATUS=$STATUS timeout --foreground --verbose --signal TERM $TIMEOUT bash -c \
     'while [[ ${STATUS_RECEIVED} != ${STATUS} ]];\
-        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${HOST}) && \
+        do STATUS_RECEIVED=$(curl -s -o /dev/null -L -w ''%{http_code}'' ${HOST});
         echo "received status: $STATUS_RECEIVED" && \
         sleep 1;\
     done;
